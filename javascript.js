5ee681e1906d202d4acbf69ba18dfffadc6cecc9
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-27 19:47:38 -0300

    Idk this kinda works or smth

--- javascript.js
+++ javascript.js
@@ -1,12 +1,27 @@
-function createGameBoard(){
+function createGameBoard() {
     const gameBoard = [
-        [ , , ,],
-        [ , , ,],
-        [ , , ,]
-      ];
-    const addO = (row, column) => gameBoard[row][column] = 'O';
-    const addX = (row, column) => gameBoard[row][column] = 'X';
+        [, , ,],
+        [, , ,],
+        [, , ,]
+    ];
+    const addMark = (playerMark, row, column) => {
+        gameBoard[row][column] = playerMark;
+    }
     const showGameBoard = () => console.log(gameBoard);
 
-    return {addO, addX, showGameBoard};
+    return { addMark, showGameBoard };
+}
+
+function createPlayer(mark, playerName) {
+    const player = playerName;
+    const playerMark = mark;
+    return { player, playerMark };
+}
+
+function createNewGame() {
+    const gameBoard = createGameBoard();
+    const playerOne = createPlayer('O', 'Jimmy');
+    const playerTwo = createPlayer('X', 'Tommy');
+
+    return { gameBoard, playerOne, playerTwo, };
 }
