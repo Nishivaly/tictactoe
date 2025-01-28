9af1e516a5407d67d35ea1043589c3815ba38980
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-28 09:56:50 -0300

    Add logic to check draws and wins

--- javascript.js
+++ javascript.js
@@ -14,6 +14,37 @@ const gameBoard = (() => {
         console.log(board);
     };
 
+    const checkWin = (mark) => {
+        for (let i = 0; i < 3; i++) {
+            if (
+                (board[i][0] === mark && board[i][1] === mark && board[i][2] === mark) || // Row
+                (board[0][i] === mark && board[1][i] === mark && board[2][i] === mark)    // Column
+            ) {
+                console.log(`${mark} wins!`)
+                return true;
+            }
+        }
+        if (
+            (board[0][0] === mark && board[1][1] === mark && board[2][2] === mark) || // Diagonal 1
+            (board[0][2] === mark && board[1][1] === mark && board[2][0] === mark)    // Diagonal 2
+        ) {
+            console.log(`${mark} wins!`)
+            return true;
+        }
+        return false;
+    };
+
+    const checkDraw = () => {
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                if (board[row][col] === null) {
+                    return false; // At least one cell is empty
+                }
+            }
+        }
+        return true; // All cells are filled
+    }
+
     const resetBoard = () => {
         for (let row = 0; row < 3; row++) {
             for (let col = 0; col < 3; col++) {
@@ -24,7 +55,7 @@ const gameBoard = (() => {
 
     const showBoard = () => console.log(board);
 
-    return { addMark, resetBoard, showBoard };
+    return { addMark, resetBoard, showBoard, checkDraw, checkWin };
 })();
 
 function createPlayer(playerName, playerMark) {
